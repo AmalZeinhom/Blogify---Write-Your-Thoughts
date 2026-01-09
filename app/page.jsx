82f0c124156e2008295565309a420a8f7c3a2944
Amal <amal.nasr.1421@gmail.com>
2026-01-09 17:35:58 +0200

    Implement ClientPage component and update dev script

--- app/page.jsx
+++ app/page.jsx
@@ -1,42 +1,11 @@
-"use client";
-
-import { useRouter, useSearchParams } from "next/navigation";
-import Categories from "./home/Categories";
-import FeaturedBlogs from "./home/FeaturedBlogs";
-import HomePage from "./home/HomePage";
-import NewsLetters from "./home/NewsLetters";
-import WhyBlogify from "./home/WhyBlogify";
-import { useEffect } from "react";
+import { Suspense } from "react";
+import ClientPage from "./ClientPage";
 
 export default function Home() {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-
-   const secretAdminKey = "allowedAccess"; 
-  const isAdminMode = searchParams.get("admin") === secretAdminKey;
-
-  useEffect(() => {
-    if (isAdminMode) {
-      sessionStorage.setItem("admin", "true");
-    }
-  }, [isAdminMode]);
 
   return (
-    <div>
-      {isAdminMode && (
-        <button
-          onClick={() => router.push("/admin/login")}
-          className="fixed top-[5%] left-[45%] z-50 opacity-50 hover:opacity-100 bg-blue-600 text-white px-4 py-2 rounded-xl"
-        >
-          Admin Login
-        </button>
-      )}
-
-      <HomePage />
-      <FeaturedBlogs />
-      <Categories />
-      <WhyBlogify />
-      <NewsLetters />
-    </div>
+    <Suspense fallback={null}>
+      <ClientPage />
+    </Suspense>
   );
 }
